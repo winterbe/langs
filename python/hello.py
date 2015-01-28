0afe67f333412e7e0cb64b8c641f34c668651255
Benjamin Winterberg <winterbe@googlemail.com>
2015-01-28 08:03:16 +0100

    python

---
+++ python/hello.py
@@ -0,0 +1,28 @@
+# Python Syntax Tutorial
+# https://docs.python.org/3/tutorial/introduction.html
+
+print 2 + 2
+
+print 0.5 * 3
+
+print 8 / 5     # int (why?)
+print 8.0 / 5   # float
+
+print 5 ** 2    # 25
+
+width = 20
+height = 5 * 8
+print width * height
+
+print "Doesn't matter"
+
+print "C:\name"
+print r"C:\name"
+
+print """\
+Hi
+There\
+"""
+
+print 3 * ("bam" + " ")
+print 3 * "he" " "
