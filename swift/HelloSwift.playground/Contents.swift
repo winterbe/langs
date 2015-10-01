d2877b0130c526f9714733337d1f65330b88b83f
Benjamin Winterberg <winterbe@gmail.com>
2015-10-01 09:11:10 +0200

    hacking swift

--- swift/HelloSwift.playground/Contents.swift
+++ swift/HelloSwift.playground/Contents.swift
@@ -23,3 +23,41 @@ func sum(a: Int, b: Int) -> Int {
 
 sum(17, b: 3)
 
+let individualScores = [45, 36, 17, 3, 99]
+var teamScore = 0
+
+for score in individualScores {
+    if (score > 50) {
+        teamScore += 3
+    } else {
+        teamScore += 1
+    }
+}
+
+print(teamScore)
+
+var optionalString : String? = "Peter"
+print(optionalString == nil)
+
+var greeting = "Hello"
+if let name = optionalString {
+    greeting += " \(name)"
+}
+
+let nickName : String? = nil
+let fullName : String = "John Doe"
+
+let informalGreeting = "Hi, \(nickName ?? fullName)"
+
+let vegetable = "red pepper"
+
+switch vegetable {
+    case "celery":
+        print("aaa")
+    case "cucumber", "watercress":
+        print("bbb")
+    case let x where x.hasSuffix("pepper"):
+        print("ddd \(x)")
+    default:
+        print("ddd")
+}
